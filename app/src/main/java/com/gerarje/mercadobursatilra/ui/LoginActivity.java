e04553bcfdb2a1409ad37661fc0de73eaaa94f40
Antonio Escorcia <ruescorcia@gmail.com>
2019-09-22 18:22:23 -0500

    Slides pantalla de inicio temporales

--- app/src/main/java/com/gerarje/mercadobursatilra/ui/LoginActivity.java
+++ app/src/main/java/com/gerarje/mercadobursatilra/ui/LoginActivity.java
@@ -7,8 +7,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.Toast;
+import android.widget.ViewFlipper;
 
 import com.gerarje.mercadobursatilra.R;
 import com.google.android.gms.auth.api.Auth;
@@ -35,11 +37,29 @@ public class LoginActivity extends AppCompatActivity implements GoogleApiClient.
 
     private SignInButton btnSignInGoogle;
     private ProgressBar progressbarLogin;
+    //objeto viewflippet
+    private ViewFlipper vf_logo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        //vector que lleva las imagenes del slide
+        int images  [] = {R.drawable.slide1, R.drawable.slide2, R.drawable.slide3};
+        //identificador por id vf logo
+        vf_logo =findViewById(R.id.vf_logo);
+
+       /* //para que se quede en loop
+        for (int i =0 ; i< images.length; i++){
+            flipperImages(images[i]);
+        }*/
+
+       //usando el foreach
+
+        for(int image: images){
+            flipperImages(image);
+        }
 
         btnSignInGoogle = findViewById(R.id.btnSignInGoogle);
         progressbarLogin = findViewById(R.id.progressbarLogin);
@@ -58,6 +78,24 @@ public class LoginActivity extends AppCompatActivity implements GoogleApiClient.
             }
         });
     }
+
+
+    public void flipperImages (int image){
+
+        ImageView imageView = new ImageView(this);
+        imageView.setBackgroundResource(image);
+        vf_logo.addView(imageView);
+        vf_logo.setFlipInterval(4000); // 4 segundos en el intervalo que va a cambiar
+        vf_logo.setAutoStart(true);
+
+
+        //la transicion
+
+        vf_logo.setInAnimation(this, android.R.anim.slide_in_left);
+        vf_logo.setInAnimation(this, android.R.anim.slide_out_right);
+    }
+
+
 
     //Inicializar la autenticación
     private void initialize(){
