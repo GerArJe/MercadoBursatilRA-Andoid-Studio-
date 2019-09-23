96412e5a243b1298272b9ddcca09aafec3632bc7
Germán Arevalo <germanarevalojerez@gmail.com>
2019-09-22 19:04:09 -0500

    modificaciones en la pantalla de inicio y arreglas de los nombres en el menu

--- app/src/main/java/com/gerarje/mercadobursatilra/ui/MenuActivity.java
+++ app/src/main/java/com/gerarje/mercadobursatilra/ui/MenuActivity.java
@@ -1,110 +1,16 @@
 package com.gerarje.mercadobursatilra.ui;
 
-import android.content.Intent;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
+
 
 import com.gerarje.mercadobursatilra.R;
-import com.google.android.gms.auth.api.Auth;
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.common.api.ResultCallback;
-import com.google.android.gms.common.api.Status;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 
-public class MenuActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener{
-
-    private static final String TAG = "MenuActivity";
-    private FirebaseAuth firebaseAuth;
-    private FirebaseAuth.AuthStateListener authStateListener;
-    private GoogleApiClient googleApiClient;
-
-    private Button btnSignOut;
+public class MenuActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
-
-        btnSignOut = findViewById(R.id.btnSignOut);
-
-        initialize();
-
-        btnSignOut.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                signOut();
-            }
-        });
-    }
-
-    //Cerrar sesión
-    private void signOut(){
-        firebaseAuth.signOut();
-        if (Auth.GoogleSignInApi != null){
-            Auth.GoogleSignInApi.signOut(googleApiClient).setResultCallback(new ResultCallback<Status>() {
-                @Override
-                public void onResult(@NonNull Status status) {
-                    if (status.isSuccess()){
-                        Intent intent = new Intent(MenuActivity.this, LoginActivity.class);
-                        startActivity(intent);
-                        finish();
-                    }else {
-                        Toast.makeText(MenuActivity.this, "Error in Google Sign Out", Toast.LENGTH_SHORT).show();
-                    }
-                }
-            });
-        }
-    }
-
-    //Inicializar la autenticación
-    private void initialize(){
-        firebaseAuth = FirebaseAuth.getInstance();
-        authStateListener = new FirebaseAuth.AuthStateListener() {
-            @Override
-            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-                FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
-                if (firebaseUser != null){
-                    Log.w(TAG, "onAuthStateChanged - signed_in" + firebaseUser.getUid());
-                    Log.w(TAG, "onAuthStateChanged - signed_in" + firebaseUser.getEmail());
-                }else {
-                    Log.w(TAG, "onAuthStateChanged - signed_out");
-                }
-            }
-        };
-
-        //Inicialización de Google Account
-        GoogleSignInOptions googleSignInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestIdToken(getString(R.string.default_web_client_id))
-                .requestEmail()
-                .build();
-        googleApiClient = new GoogleApiClient.Builder(this)
-                .enableAutoManage(this, this)
-                .addApi(Auth.GOOGLE_SIGN_IN_API, googleSignInOptions)
-                .build();
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        firebaseAuth.addAuthStateListener(authStateListener);
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        firebaseAuth.removeAuthStateListener(authStateListener);
-    }
-
-    @Override
-    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-
     }
 }
